f3dcfd457295fbcb394fbbf6cb78c5a57b23a849
Nestor Eduartes <neduarteslar@mail.usf.edu>
2015-03-25 14:08:21 -0400

    First functional dynamic block definition

--- dynamic_block.m
+++ dynamic_block.m
@@ -1,36 +1,16 @@
-function msfuntmpl_basic(block)
-%MSFUNTMPL_BASIC A Template for a Level-2 MATLAB S-Function
-%   The MATLAB S-function is written as a MATLAB function with the
-%   same name as the S-function. Replace 'msfuntmpl_basic' with the 
-%   name of your S-function.
-%
-%   It should be noted that the MATLAB S-function is very similar
-%   to Level-2 C-Mex S-functions. You should be able to get more
-%   information for each of the block methods by referring to the
-%   documentation for C-Mex S-functions.
-%
-%   Copyright 2003-2010 The MathWorks, Inc.
-
-%%
-%% The setup method is used to set up the basic attributes of the
-%% S-function such as ports, parameters, etc. Do not add any other
-%% calls to the main body of the function.
-%%
+function dynamic_block(block)
+% dynamic_block is a Level-2 MATLAB S-Function for implementing
+% a quadrotor nonlinear dynamic model in SIMULINK
 setup(block);
-
 %endfunction
 
 %% Function: setup ===================================================
 %% Abstract:
 %%   Set up the basic characteristics of the S-function block such as:
 %%   - Input ports
-%%   - Output ports
-%%   - Dialog parameters
-%%   - Options
-%%
-%%   Required         : Yes
-%%   C-Mex counterpart: mdlInitializeSizes
-%%
+%      The block handles two inputs:
+%       - System states
+%       - System inputs
 function setup(block)
 
 % Register number of ports
@@ -42,71 +22,38 @@ block.SetPreCompInpPortInfoToDynamic;
 block.SetPreCompOutPortInfoToDynamic;
 
 % Override input port properties
-block.InputPort(1).Dimensions        = 1;
+block.InputPort(1).Dimensions  = 4;
 block.InputPort(1).DatatypeID  = 0;  % double
 block.InputPort(1).Complexity  = 'Real';
-block.InputPort(1).DirectFeedthrough = true;
+block.InputPort(1).DirectFeedthrough = false;
 
 % Override output port properties
-block.OutputPort(1).Dimensions       = 1;
+block.OutputPort(1).Dimensions  = 6;
 block.OutputPort(1).DatatypeID  = 0; % double
 block.OutputPort(1).Complexity  = 'Real';
 
 % Register parameters
-block.NumDialogPrms     = 0;
+block.NumDialogPrms     = 1;
 
 % Register sample times
-%  [0 offset]            : Continuous sample time
-%  [positive_num offset] : Discrete sample time
-%
-%  [-1, 0]               : Inherited sample time
-%  [-2, 0]               : Variable sample time
-block.SampleTimes = [0 0];
+block.SampleTimes = [0.001 0];
 
-% Specify the block simStateCompliance. The allowed values are:
-%    'UnknownSimState', < The default setting; warn and assume DefaultSimState
-%    'DefaultSimState', < Same sim state as a built-in block
-%    'HasNoSimState',   < No sim state
-%    'CustomSimState',  < Has GetSimState and SetSimState methods
-%    'DisallowSimState' < Error out when saving or restoring the model sim state
-block.SimStateCompliance = 'DefaultSimState';
-
-%% -----------------------------------------------------------------
-%% The MATLAB S-function uses an internal registry for all
-%% block methods. You should register all relevant methods
-%% (optional and required) as illustrated below. You may choose
-%% any suitable name for the methods and implement these methods
-%% as local functions within the same file. See comments
-%% provided for each function for more information.
-%% -----------------------------------------------------------------
-
-block.RegBlockMethod('PostPropagationSetup',    @DoPostPropSetup);
-block.RegBlockMethod('InitializeConditions', @InitializeConditions);
-block.RegBlockMethod('Start', @Start);
-block.RegBlockMethod('Outputs', @Outputs);     % Required
-block.RegBlockMethod('Update', @Update);
-block.RegBlockMethod('Derivatives', @Derivatives);
-block.RegBlockMethod('Terminate', @Terminate); % Required
-
+block.RegBlockMethod('PostPropagationSetup', @DoPostPropSetup);
+block.RegBlockMethod('InitializeConditions', @InitConditions);
+block.RegBlockMethod('Outputs',              @Outputs);
+block.RegBlockMethod('Update',               @Update);
 %end setup
 
-%%
 %% PostPropagationSetup:
-%%   Functionality    : Setup work areas and state variables. Can
-%%                      also register run-time methods here
-%%   Required         : No
-%%   C-Mex counterpart: mdlSetWorkWidths
-%%
+%  Discrete state definition
 function DoPostPropSetup(block)
 block.NumDworks = 1;
-  
-  block.Dwork(1).Name            = 'x1';
-  block.Dwork(1).Dimensions      = 1;
+  block.Dwork(1).Name            = 'state';
+  block.Dwork(1).Dimensions      = 6;
   block.Dwork(1).DatatypeID      = 0;      % double
   block.Dwork(1).Complexity      = 'Real'; % real
   block.Dwork(1).UsedAsDiscState = true;
-
-
+  
 %%
 %% InitializeConditions:
 %%   Functionality    : Called at the start of simulation and if it is 
@@ -116,35 +63,15 @@ block.NumDworks = 1;
 %%   Required         : No
 %%   C-MEX counterpart: mdlInitializeConditions
 %%
-function InitializeConditions(block)
-
+function InitConditions(block)
+block.Dwork(1).Data = block.DialogPrm(1).Data;
 %end InitializeConditions
-
-
-%%
-%% Start:
-%%   Functionality    : Called once at start of model execution. If you
-%%                      have states that should be initialized once, this 
-%%                      is the place to do it.
-%%   Required         : No
-%%   C-MEX counterpart: mdlStart
-%%
-function Start(block)
-
-block.Dwork(1).Data = 0;
-
-%end Start
 
 %%
 %% Outputs:
-%%   Functionality    : Called to generate block outputs in
-%%                      simulation step
-%%   Required         : Yes
-%%   C-MEX counterpart: mdlOutputs
-%%
 function Outputs(block)
 
-block.OutputPort(1).Data = block.Dwork(1).Data + block.InputPort(1).Data;
+block.OutputPort(1).Data = block.Dwork(1).Data;
 
 %end Outputs
 
@@ -156,29 +83,10 @@ block.OutputPort(1).Data = block.Dwork(1).Data + block.InputPort(1).Data;
 %%   C-MEX counterpart: mdlUpdate
 %%
 function Update(block)
-
-block.Dwork(1).Data = block.InputPort(1).Data;
+block.Dwork(1).Data = rotorDynamics(block.Dwork(1).Data, block.InputPort(1).Data, 0.001);
 
 %end Update
 
-%%
-%% Derivatives:
-%%   Functionality    : Called to update derivatives of
-%%                      continuous states during simulation step
-%%   Required         : No
-%%   C-MEX counterpart: mdlDerivatives
-%%
-function Derivatives(block)
-
-%end Derivatives
-
-%%
-%% Terminate:
-%%   Functionality    : Called at the end of simulation for cleanup
-%%   Required         : Yes
-%%   C-MEX counterpart: mdlTerminate
-%%
 function Terminate(block)
 
 %end Terminate
-
