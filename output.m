717ae27c30370bd4bc34a16b83a13aa00506f432
Nestor Eduartes <neduarteslar@mail.usf.edu>
2015-04-07 14:20:35 -0400

    Figures and results included

--- output.m
+++ output.m
@@ -1,9 +1,22 @@
 figure(1)
 
-for n =1:3
-    subplot(3,1,n)
-    plot(time, output.signals.values(:,2*n - 1),'-b', time, output.signals.values(:,2*n), '-r');
-end
+subplot(3,1,1)
+plot(time, output.signals.values(:,1),'-b');
+xlabel('Time [sec]')
+axis([0 5 -2 2])
+grid on
+
+subplot(3,1,2)
+plot(time, output.signals.values(:,3),'-r');
+xlabel('Time [sec]')
+axis([0 5 -2 2])
+grid on
+
+subplot(3,1,1)
+plot(time, output.signals.values(:,5),'-g');
+xlabel('Time [sec]')
+axis([0 5 -2 2])
+grid on
 
 figure(2)
 for n =1:3
