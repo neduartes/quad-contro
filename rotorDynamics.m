6ba6b30a06475d204b5f52a7ce61ab33d4de1603
Nestor Eduartes <neduarteslar@mail.usf.edu>
2015-04-14 18:06:00 -0400

    Critical update of quadrotor dynamic model

--- rotorDynamics.m
+++ rotorDynamics.m
@@ -16,21 +16,21 @@ function [state_fut] = rotorDynamics(state, input, step)
 
 
     thrust = input .^ 2;
-    U1 = b * arm * [ 0 -1 0 1] * thrust;
-    U2 = b * arm * [-1  0 1 0] * thrust;
-    U3 = d * arm * [ 1 -1 1 -1] * thrust;
+    U1 = b * [ 0 -1 0 1] * thrust;
+    U2 = b * [-1  0 1 0] * thrust;
+    U3 = d * [ 1 -1 1 -1] * thrust;
     U = [-1 1 -1 1] * thrust;
     
     state_fut = zeros(6,1);
     
     state_fut(1) = state(1) + state(2) * step;
-    state_fut(2) = state(2) + state(4) * state(6) * (Iy - Iz) * step / Ix + ...
-        Prop_inertia * state(2) * U * step / Ix + arm * U1 * step / Ix;
+    state_fut(2) = state(2) + state(4) * state(6) * (Iy - Iz) * step / Ix - ...
+        Prop_inertia * state(4) * U * step / Ix + arm * U1 * step / Ix;
     state_fut(3) = state(3) + state(4) * step;
     state_fut(4) = state(4) + state(2) * state(6) * (Iz - Ix) * step / Iy + ...
         Prop_inertia * state(2) * U * step / Iy + arm * U2 * step / Iy;
     state_fut(5) = state(5) + state(6) * step;
     state_fut(6) = state(6) + state(2) * state(4) * (Ix - Iy) * step / Iz + ...
-        U3 / Iz;
+        U3 * step / Iz;
     return;
 end
